2547f8d4d981d99fe157391a0f5c642f72f406b6
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2023-07-11 15:53:55 -0300

    refactor: separating form validations

--- src/components/RegisterForm/index.tsx
+++ src/components/RegisterForm/index.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { validateEmail } from 'utils/validations';
+import { userName, email, password } from 'utils/validations';
 import { z } from 'zod';
 
 import { RegisterFormContainer } from './styles';
@@ -13,23 +13,9 @@ import Button from 'components/Button';
 import Input from 'components/Input';
 
 const registerFormSchema = z.object({
-  userName: z
-    .string()
-    .nonempty('Esse campo é obrigatório.')
-    .min(3, 'Seu nome de usuário deve conter pelo menos 3 caracteres.')
-    .trim(),
-
-  email: z
-    .string()
-    .nonempty('Esse campo é obrigatório.')
-    .email('Email inválido.')
-    .regex(validateEmail, 'Email inválido.'),
-
-  password: z
-    .string()
-    .nonempty('Esse campo é obrigatório.')
-    .min(6, 'A senha deve ter entre 6 e 60 caracteres.')
-    .max(60, 'A senha deve ter entre 6 e 60 caracteres.'),
+  userName,
+  email,
+  password,
 });
 
 type RegisterFormData = z.infer<typeof registerFormSchema>;
--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,2 +1,22 @@
-export const validateEmail =
+import { z } from 'zod';
+
+const emailRegEx =
   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+export const userName = z
+  .string()
+  .nonempty('Esse campo é obrigatório.')
+  .min(3, 'Seu nome de usuário deve conter pelo menos 3 caracteres.')
+  .trim();
+
+export const email = z
+  .string()
+  .nonempty('Esse campo é obrigatório.')
+  .email('Email inválido.')
+  .regex(emailRegEx, 'Email inválido.');
+
+export const password = z
+  .string()
+  .nonempty('Esse campo é obrigatório.')
+  .min(6, 'A senha deve ter entre 6 e 60 caracteres.')
+  .max(60, 'A senha deve ter entre 6 e 60 caracteres.');
