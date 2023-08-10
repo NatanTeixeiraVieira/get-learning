6b99b68405ae50d82e91c7a2ded0a6c736b2061a
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2023-08-10 10:41:50 -0300

    chore: Add ok in fetcher response

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -5,7 +5,7 @@ const fetcher = async <T = unknown>(
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${input}`, init);
   const datas: T = await res.json();
 
-  return { datas, status: res.status };
+  return { datas, status: res.status, ok: res.ok };
 };
 
 export default fetcher;
