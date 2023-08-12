93759951f19458d58bcbf567126691347e2f3a98
Natãn Teixeira Vieira <natanteixeiravieira@gmail.com>
2023-08-12 14:17:11 -0300

    feat: Add responsiveness in login page

--- src/app/login/styles.ts
+++ src/app/login/styles.ts
@@ -5,16 +5,32 @@ import styled, { css } from 'styled-components';
 export const Container = styled.div`
   ${({ theme }) => css`
     padding: ${theme.spacings.medium};
+
+    ${theme.media.mobile} {
+      padding: ${theme.spacings.xsmall};
+    }
   `}
 `;
 
 export const LoginContent = styled.div`
-  width: 35rem;
-  margin: 0 auto;
+  ${({ theme }) => css`
+    width: 35rem;
+    margin: 0 auto;
+
+    ${theme.media.mobile} {
+      width: 100%;
+    }
+  `}
 `;
 
 export const UtilsPages = styled.div`
   ${({ theme }) => css`
     margin-top: ${theme.spacings.small};
+
+    p {
+      ${theme.media.mobile} {
+        margin: ${theme.spacings.xxxsmall} 0;
+      }
+    }
   `}
 `;
--- src/components/LoginForm/style.ts
+++ src/components/LoginForm/style.ts
@@ -2,11 +2,19 @@
 
 import styled, { css } from 'styled-components';
 
+import { ButtonContainer } from 'components/Button/styles';
+
 export const LoginFormContainer = styled.form`
   ${({ theme }) => css`
     display: flex;
     flex-direction: column;
     gap: ${theme.spacings.xsmall};
     margin-top: ${theme.spacings.small};
+
+    ${theme.media.mobile} {
+      ${ButtonContainer} {
+        margin: ${theme.spacings.xsmall} 0;
+      }
+    }
   `}
 `;
