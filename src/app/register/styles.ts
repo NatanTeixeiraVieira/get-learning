a8bfe4bf5809558ae927426b5e688dad5bb374b7
Natãn Teixeira Vieira <natanteixeiravieira@gmail.com>
2023-08-12 14:17:44 -0300

    feat: Add responsiveness in register page

--- src/app/register/styles.ts
+++ src/app/register/styles.ts
@@ -5,10 +5,20 @@ import styled, { css } from 'styled-components';
 export const Container = styled.div`
   ${({ theme }) => css`
     padding: ${theme.spacings.medium};
+
+    ${theme.media.mobile} {
+      padding: ${theme.spacings.xsmall};
+    }
   `}
 `;
 
 export const RegisterContent = styled.div`
-  width: 35rem;
-  margin: 0 auto;
+  ${({ theme }) => css`
+    max-width: 35rem;
+    margin: 0 auto;
+
+    ${theme.media.mobile} {
+      width: 100%;
+    }
+  `}
 `;
