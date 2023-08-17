1d7f9c9cbce78608b998dc55bb0fc7e647010fb3
Natãn Teixeira Vieira <natanteixeiravieira@gmail.com>
2023-08-16 21:39:31 -0300

    feat: Add delete and edit icon when is post owner

--- src/components/PostCard/index.tsx
+++ src/components/PostCard/index.tsx
@@ -1,30 +1,54 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+import { Pencil } from 'lucide-react';
+import { CoverImage } from 'types/post';
 import { textFormatter } from 'utils/textFormatter';
 
-import { Container, Subtitle } from './styles';
+import { Container, PostImage, Options, Subtitle, UpdatePost } from './styles';
 
+import DeletePost from 'components/DeletePost';
 import Heading from 'components/Heading';
 
 export type PostCardProps = {
   title: string;
   subtitle: string;
-  imageSrc: string;
+  coverImage: CoverImage;
   id: string;
+  isPostOwner: boolean;
 };
 
 export default function PostCard({
   title,
   subtitle,
-  imageSrc,
+  coverImage,
   id,
+  isPostOwner,
 }: PostCardProps) {
   return (
     <Container>
-      <Link href={`/post/${id}`}>
-        <Image src={imageSrc} alt={title} width={320} height={180} />
-      </Link>
+      {isPostOwner && (
+        <Options>
+          <UpdatePost>
+            <Link
+              href={{
+                pathname: '/edit',
+                query: {
+                  update: id,
+                },
+              }}
+            >
+              <Pencil size="1rem" />
+            </Link>
+          </UpdatePost>
+          <DeletePost postId={id} coverImageName={coverImage.name} />
+        </Options>
+      )}
+      <PostImage>
+        <Link href={`/post/${id}`}>
+          <Image src={coverImage.url} alt={title} width={320} height={180} />
+        </Link>
+      </PostImage>
       <Heading as="h2" size="small">
         <Link href={`/post/${id}`}>{title}</Link>
       </Heading>
