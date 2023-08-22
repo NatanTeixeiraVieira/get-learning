437bc2a59a6dae343eff66e97a5b12e90f84fe80
Natãn Teixeira Vieira <natanteixeiravieira@gmail.com>
2023-08-21 23:21:37 -0300

    refactor: changing param name and inverting logic

--- src/utils/textFormatter.ts
+++ src/utils/textFormatter.ts
@@ -1,5 +1,8 @@
-export const textFormatter = (description: string, characters = 100) => {
-  return description.length < characters
-    ? description
-    : description.substring(0, characters).concat('...');
+export const textFormatter = (text?: string, characters = 100) => {
+  if (text) {
+    return text.length > characters
+      ? text.substring(0, characters).concat('...')
+      : text;
+  }
+  return '';
 };
