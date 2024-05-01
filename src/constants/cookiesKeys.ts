7dd4429fd67ff1ef3559112777c5336a327df0d8
NatanTeixeiraVieira <natanteixeiravieira@gmail.com>
2024-05-01 14:02:03 -0300

    chore: Adjust toggle theme

--- src/constants/cookiesKeys.ts
+++ src/constants/cookiesKeys.ts
@@ -2,3 +2,4 @@ const baseName = 'get-learning';
 export const emailIdKey = `${baseName}.emailId`;
 export const tokenKey = `${baseName}.token`;
 export const userKey = `${baseName}.user`;
+export const themeNameKey = `${baseName}.theme`;
--- src/hooks/useToggleTheme.tsx
+++ src/hooks/useToggleTheme.tsx
@@ -1,27 +1,69 @@
-// import useThemeStore from 'store/theme';
+import { useCallback } from 'react';
 
-// import dark from 'styles/themes/dark';
-// import light from 'styles/themes/light';
+import { themeNameKey } from 'constants/cookiesKeys';
+import { parseCookies, setCookie } from 'nookies';
+import { useThemeStore } from 'store/theme';
+import { Theme, ThemeNames } from 'types/theme';
 
-// export const useToggleTheme = () => {
-//   const { theme } = useThemeStore().state;
-//   const isDefaultDarkTheme = window.matchMedia(
-//     '(prefers-color-scheme: dark)'
-//   ).matches;
-//   let choosenTheme: typeof light | typeof dark = light;
+import dark from 'styles/themes/dark';
+import light from 'styles/themes/light';
 
-//   switch (theme) {
-//     case 'light':
-//       choosenTheme = light;
-//       break;
-//     case 'dark':
-//       choosenTheme = dark;
-//       break;
-//     case 'system':
-//       if (isDefaultDarkTheme) {
-//         choosenTheme = dark;
-//       }
-//       break;
-//   }
-//   return choosenTheme;
-// };
+export const useToggleTheme = () => {
+  const {
+    state: { theme },
+    actions: { setTheme },
+  } = useThemeStore();
+
+  const startTheme = useCallback(() => {
+    const themeName = parseCookies()[themeNameKey] as ThemeNames | null;
+
+    if (themeName === 'light') {
+      setTheme(light);
+      return;
+    }
+    if (themeName === 'dark') {
+      setTheme(dark);
+      return;
+    }
+
+    const isDefaultDarkTheme = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches;
+
+    if (isDefaultDarkTheme) {
+      setTheme(dark);
+      return;
+    }
+
+    setTheme(light);
+  }, [setTheme]);
+
+  const toggleTheme = useCallback(
+    (theme: Theme | null) => {
+      if (!theme) {
+        setCookie(null, themeNameKey, 'system');
+        const isDefaultDarkTheme = window.matchMedia(
+          '(prefers-color-scheme: dark)'
+        ).matches;
+
+        if (isDefaultDarkTheme) {
+          setTheme(dark);
+          return;
+        }
+
+        setTheme(light);
+        return;
+      }
+
+      setTheme(theme);
+      setCookie(null, themeNameKey, theme.title);
+    },
+    [setTheme]
+  );
+
+  return {
+    theme,
+    startTheme,
+    toggleTheme,
+  };
+};
